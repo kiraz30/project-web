3710f9874b7c1c5eb1c487da46723dd673fe8946
kiraz30 <setyabima016@gmail.com>
2023-12-03 20:20:41 +0700

    create routing response json

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -14,9 +16,9 @@ type config struct {
 }
 
 type AppStatus struct {
-	Status      string `json:"status`
+	Status      string `json:"status"`
 	Environment string `json:"environment"`
-	Version     string `json:"version`
+	Version     string `json:"version"`
 }
 
 func main() {
@@ -29,5 +31,25 @@ func main() {
 	fmt.Println("Server is running...")
 
 	//routing
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {})
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		currentStatus := AppStatus{
+			Status:      "Online",
+			Environment: cfg.env,
+			Version:     version,
+		}
+
+		res, err := json.MarshalIndent(currentStatus, "", "\t")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
