b535c2af86c4b522fabcdcaf208a39c57c218da1
kiraz30 <setyabima016@gmail.com>
2023-12-07 19:28:14 +0700

    CREATE util and get oneMovie

---
+++ server/cmd/api/movieHandler.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"project-web/models"
+	"strconv"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("invalid id parameter"))
+	}
+
+	app.logger.Println("ID", id)
+
+	movie := models.Movie{
+		ID:          id,
+		Title:       "Warkop Dki",
+		Description: "Comedy movie",
+		Year:        2010,
+		ReleaseDate: time.Date(1990, 04, 30, 0, 0, 0, 0, time.Local),
+		Runtime:     120,
+		Rating:      5,
+		MPAARating:  "PG-12",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	err = app.writeJson(w, http.StatusOK, movie, "movie")
+}
